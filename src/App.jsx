f55a88c9fd70ab3dec1b2de92166238392207408
Achira Dimuthu <bvachiradimuthu@gmail.com>
2024-06-10 18:14:43 +0530

    Implemented skills section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Header from "./header/Header";
 import About from "./sections/about/About";
+import Skills from "./sections/akills/Skills";
 
 function App() {
   return (
     <>
       <Header />
       <About />
+      <Skills />
     </>
   );
 }
